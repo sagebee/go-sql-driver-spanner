e693f49c219e1d0271b4f24c81a359d0821f85fb
Sage Barreda-pitcairn <z5135742@ad.unsw.edu.au>
2021-01-28 13:22:09 +1100

    null read issue investigatoin

--- issues/issue03.go
+++ issues/issue03.go
@@ -8,77 +8,195 @@ import (
 	"fmt"
 	"log"
 	//"time"
-	"os"
 	"cloud.google.com/go/spanner"
+	"os"
 
 	_ "github.com/rakyll/go-sql-driver-spanner"
 
-		// API/lib packages not imported by driver.
+	// API/lib packages not imported by driver.
 	adminapi "cloud.google.com/go/spanner/admin/database/apiv1"
 	"google.golang.org/api/option"
 	adminpb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
 	"google.golang.org/grpc"
 )
 
-var(
+var (
 	dsn = "projects/test-project/instances/test-instance/databases/gotest"
 )
 
 func main() {
 
-	// Build table.  
+	// Build table.
 	curs, err := NewConnector()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer curs.Close()
 
+	// Open db.
+	ctx := context.Background()
+	db, err := sql.Open("spanner", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	executeDdlApi(curs, []string{`CREATE TABLE TestQueryType (
+		key	STRING(1024),
 		stringt	STRING(1024),
-		bytest BYTES(1024),
 		intt  INT64,
 		floatt   FLOAT64,
 		boolt BOOL
 	)	 PRIMARY KEY (stringt)`})
 
-	ExecuteDMLClientLib([]string{`INSERT INTO TestQueryType (stringt,bytest ,intt, floatt, boolt) 
-		VALUES ("aa", CAST("aa" as bytes), 42, 42, TRUE), ("bb", CAST("bb" as bytes),-42, -42, FALSE),
-		("nullbytes", null, 42, 42, TRUE),
-		("nullint", CAST("nullint" as bytes), null, 42, TRUE),
-		("nullfloat", CAST("nullfloat" as bytes), 42, null, TRUE),
-		("nullbool", CAST("nullbool" as bytes), 42, 42, null)`})
+	err = ExecuteDMLClientLib([]string{`INSERT INTO TestQueryType (key, stringt,intt, floatt, boolt) 
+		VALUES ("null", null ,null, null, null)`})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 
-		// null bytes
 
-		type  atomicRow struct {
-			stringt string
-			bytest  []byte // Small byte buffer
-			intt    int
-			floatt  float64
-			boolt   bool
+	ReadIntoRegular(db,ctx)
+	ReadIntoPointers(db,ctx)
+	ReadIntoNullable(db, ctx)
+	ReadIntoNullablePointer(db, ctx)
+
+
+	// drop table
+	executeDdlApi(curs, []string{`DROP TABLE TestQueryType`})
+}
+
+func ReadIntoRegular(db *sql.DB, ctx context.Context){
+
+	type regularRow struct {
+		key     string
+		stringt string
+		intt    int
+		floatt  float64
+		boolt   bool
+	}
+
+	// filled w junk to see if it overrides or leaves as is
+	curr := regularRow{stringt :"hi", intt: 42, floatt: 42, boolt: true}
+
+	rows, err := db.QueryContext(ctx, "SELECT * FROM TestQueryType where key = 'null'")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for rows.Next() {
+		if err := rows.Scan(&curr.key, &curr.stringt, &curr.intt, &curr.floatt, &curr.boolt); err != nil {
+			log.Fatal(err.Error())
 		}
+	}
+	rows.Close()
 
-		curr := atomicRow{}
+	fmt.Println("\nRead null vals from database into regular typess>")
+	fmt.Printf("string: %s int: %d float %f bool: %t\n",
+		curr.stringt, curr.intt, curr.floatt, curr.boolt)
+	fmt.Printf("\n\n")
 
-		rows, err := db.QueryContext(ctx, "SELECT * FROM SpecialFloat")
+}
+
+func ReadIntoPointers(db *sql.DB, ctx context.Context){
+
+	type pointerRow struct {
+		key     string
+		stringt *string
+		intt    *int
+		floatt  *float64
+		boolt   *bool
+	}
+
+	dnum := 42
+	fnum := 42.0
+	tru := true
+	strin := "hi"
+	curr := pointerRow{stringt: &strin, intt: &dnum, floatt: &fnum, boolt: &tru}
+
+	rows, err := db.QueryContext(ctx, "SELECT * FROM TestQueryType where key = 'null'")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for rows.Next() {
+		if err := rows.Scan(&curr.key, &curr.stringt, &curr.intt, &curr.floatt, &curr.boolt); err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+	rows.Close()
+
+	fmt.Println("Read null vals from database into pointers>")
+	fmt.Printf("string: %s int: %d float %f bool: %t\n",
+		*curr.stringt, *curr.intt, *curr.floatt, *curr.boolt)
+	fmt.Printf("\n\n")
+}
+
+func ReadIntoNullablePointer(db *sql.DB, ctx context.Context){
+
+	type nullableRow struct {
+		key     string
+		stringt *sql.NullString
+		intt    *sql.NullInt64
+		floatt  *sql.NullFloat64
+		boolt   *sql.NullBool
+	}
+
+		// read null into sql nullab;e vals 
+		nint := sql.NullInt64{Int64: 0, Valid:false}
+		nstrin := sql.NullString{String:"hi", Valid:false}
+		currn := nullableRow{intt: &nint, stringt: &nstrin}
+
+
+		rows, err := db.QueryContext(ctx, "SELECT * FROM TestQueryType where key = 'null'")
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-	
 		for rows.Next() {
-			if err := rows.Scan(&curr.id, &curr.notnum, &curr.pinf, &curr.ninf); err != nil {
+			if err := rows.Scan(&currn.key, &currn.stringt, &currn.intt, &currn.floatt, &currn.boolt); err != nil {
 				log.Fatal(err.Error())
 			}
 		}
 		rows.Close()
+	
+		fmt.Println("Read null vals from database into sql nullable struct with pointers>")
+		fmt.Printf("string: %v int: %v float %v bool: %v\n",
+			*currn.stringt, *currn.intt, *currn.floatt, *currn.boolt)
+		fmt.Printf("\n\n")
 
-
-
-
-		// drop table 
-		executeDdlApi(curs, []string{`DROP TABLE TestQueryType`})
 }
+
+func ReadIntoNullable(db *sql.DB, ctx context.Context){
+
+	type nullableRow struct {
+		key     string
+		stringt sql.NullString
+		intt    sql.NullInt64
+		floatt  sql.NullFloat64
+		boolt   sql.NullBool
+	}
+
+		// read null into sql nullab;e vals 
+		currn := nullableRow{intt: sql.NullInt64{Int64: 0, Valid:false}, stringt: sql.NullString{String:"hi", Valid:false}}
+
+
+		rows, err := db.QueryContext(ctx, "SELECT * FROM TestQueryType where key = 'null'")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		for rows.Next() {
+			if err := rows.Scan(&currn.key, &currn.stringt, &currn.intt, &currn.floatt, &currn.boolt); err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+		rows.Close()
+	
+		fmt.Println("Read null vals from database into sql nullable struct without pointers>")
+		fmt.Printf("string: %v int: %v float %v bool: %v\n",
+			currn.stringt, currn.intt, currn.floatt, currn.boolt)
+		fmt.Printf("\n\n")
+
+}
+
 
 type Connector struct {
 	ctx         context.Context
@@ -132,7 +250,6 @@ func (c *Connector) Close() {
 	c.adminClient.Close()
 }
 
-
 // Executes DDL statements.
 func executeDdlApi(curs *Connector, ddls []string) (err error) {
 
@@ -152,8 +269,6 @@ func executeDdlApi(curs *Connector, ddls []string) (err error) {
 // Executes DML using the client library.
 func ExecuteDMLClientLib(dml []string) (err error) {
 
-	dsn := "projects/test-project/instances/test-instance/databass/gotest"
-
 	// Open client/
 	ctx := context.Background()
 	client, err := spanner.NewClient(ctx, dsn)
@@ -171,11 +286,10 @@ func ExecuteDMLClientLib(dml []string) (err error) {
 	// Execute statements.
 	_, err = client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
 		stmts := stmts
-		rowCounts, err := txn.BatchUpdate(ctx, stmts)
+		_, err := txn.BatchUpdate(ctx, stmts)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("Executed %d SQL statements using Batch DML.\n", len(rowCounts))
 		return nil
 	})
 
